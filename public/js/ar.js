1221e075bd197a9817b3cdd664e4f1a4faea2a6a
mikheyrojo <mikhail.stepanov@starship.co>
2023-09-27 09:33:10 +0300

    audio tracks

--- public/js/ar.js
+++ public/js/ar.js
@@ -1,21 +1,21 @@
 const initDeepAr = async () => {
 
   console.log({isInitiator});
+  const canvas = document.querySelector('canvas');
 
 
   const deepAR = await deepar.initialize({
+    canvas,
     licenseKey: 'f46f49ed78f2a4bfe2d6775a29be434a354fcbb433c8df4d785ef7cb99f092624d56cf84b58c8b9a',
-    previewElement: document.querySelector('#deepar-div'),
+    // previewElement: document.querySelector('#deepar-div'),
     effect: 'https://cdn.jsdelivr.net/npm/deepar/effects/aviators',
   }).catch(error => {
     console.log(error);
   });
 
-  deepAR.callbacks.onFaceVisibilityChanged = (args) => {
-    // console.log(args); // true / false
-  };
+  await deepAR.backgroundReplacement(true, 'images/bg.jpeg');
 
-  const canvas = document.querySelector('canvas');
+
   const remoteVideo = document.querySelector('#localVideo');
   console.log('my strem is ready')
   const arStream = canvas.captureStream();
