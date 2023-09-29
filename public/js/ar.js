c5f05da2489404f62c94e87eb93cb0b2f8c4b709
mikheyrojo <mikhail.stepanov@starship.co>
2023-09-29 15:32:31 +0300

    pitch

--- public/js/ar.js
+++ public/js/ar.js
@@ -27,15 +27,19 @@ const initDeepAr = async () => {
   navigator.mediaDevices.getUserMedia({audio: true, video: true})
     .then((stream) => {
 
-      const audioContext = new AudioContext();
-      const mediaStreamSource = audioContext.createMediaStreamSource(stream);
-      const mediaStreamDestination = audioContext.createMediaStreamDestination();
-      const oscillator = audioContext.createOscillator();
+      const audioCtx = new AudioContext();
+      const mediaStreamSource = audioCtx.createMediaStreamSource(stream);
+      const mediaStreamDestination = audioCtx.createMediaStreamDestination();
 
-      oscillator.type = "square";
-      oscillator.start()
-      mediaStreamSource.connect(mediaStreamDestination);
-      oscillator.connect(mediaStreamDestination);
+      const filter = getFilter(audioCtx);
+
+      console.log({filter})
+
+
+
+      mediaStreamSource.connect(filter);
+
+      filter.connect(mediaStreamDestination);
 
 
       const destinationTracks = mediaStreamDestination.stream.getAudioTracks();
@@ -50,3 +54,71 @@ const initDeepAr = async () => {
 
 
 }
+
+const hannWindow = function (length) {
+
+  var window = new Float32Array(length);
+  for (var i = 0; i < length; i++) {
+    window[i] = 0.5 * (1 - Math.cos(2 * Math.PI * i / (length - 1)));
+  }
+  return window;
+};
+
+const getFilter =(audioContext) => {
+  const grainSize = 512;
+  const pitchRatio = 2;
+  const overlapRatio = 0.5;
+
+  const pitchShifterProcessor = audioContext.createScriptProcessor(grainSize, 1, 1);
+  pitchShifterProcessor.buffer = new Float32Array(grainSize * 2);
+  pitchShifterProcessor.grainWindow = hannWindow(grainSize);
+
+  pitchShifterProcessor.onaudioprocess = function (event) {
+
+    var inputData = event.inputBuffer.getChannelData(0);
+    var outputData = event.outputBuffer.getChannelData(0);
+
+    for (i = 0; i < inputData.length; i++) {
+
+      // Apply the window to the input buffer
+      inputData[i] *= this.grainWindow[i];
+
+      // Shift half of the buffer
+      this.buffer[i] = this.buffer[i + grainSize];
+
+      // Empty the buffer tail
+      this.buffer[i + grainSize] = 0.0;
+    }
+
+    // Calculate the pitch shifted grain re-sampling and looping the input
+    var grainData = new Float32Array(grainSize * 2);
+    for (var i = 0, j = 0.0;
+         i < grainSize;
+         i++, j += pitchRatio) {
+
+      var index = Math.floor(j) % grainSize;
+      var a = inputData[index];
+      var b = inputData[(index + 1) % grainSize];
+      grainData[i] += linearInterpolation(a, b, j % 1.0) * this.grainWindow[i];
+    }
+
+    // Copy the grain multiple times overlapping it
+    for (i = 0; i < grainSize; i += Math.round(grainSize * (1 - overlapRatio))) {
+      for (j = 0; j <= grainSize; j++) {
+        this.buffer[i + j] += grainData[j];
+      }
+    }
+
+    // Output the first half of the buffer
+    for (i = 0; i < grainSize; i++) {
+      outputData[i] = this.buffer[i];
+    }
+  };
+
+  return pitchShifterProcessor;
+}
+
+
+const linearInterpolation = function (a, b, t) {
+  return a + (b - a) * t;
+};
